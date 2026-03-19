0ae4cd5209129d65a6695b9fb0c6831cbe6a8a52
Fanelock <colin.kreienbuehl@uzh.ch>
2026-03-19 12:59:44 +0100

    added prod-url, implemented-group name-display

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <h1>FS26 Group 31</h1>
         <ol>
           <li>
             <code>app/page.tsx</code>{" "}
